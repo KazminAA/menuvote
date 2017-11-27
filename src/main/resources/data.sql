5d6b74ab8995dc0332c65ee9ab5fb8a5ad4c9728
Alexandr <lleeexxx@gmail.com>
2017-11-27 14:52:57 +0200

    add scripts to initialize and pooulate database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,48 @@
+DELETE FROM user_roles;
+DELETE FROM users;
+ALTER SEQUENCE global_seq RESTART WITH 100000;
+
+INSERT INTO users (name, email, password) VALUES
+  ('User', 'user@yandex.ru', 'password'),
+  ('User2', 'user2@yandex.ru', 'password'),
+  ('Admin', 'admin@gmail.com', 'admin');
+
+INSERT INTO user_roles (role, user_id) VALUES
+  ('ROLE_USER', 100000),
+  ('ROLE_USER', 100001),
+  ('ROLE_ADMIN', 100002);
+
+INSERT INTO restaurants (name) VALUES
+  ('Арчибальд'),
+  ('YellowSubmarine'),
+  ('Словянка');
+
+INSERT INTO menus (date, curvotenum, restaurant_id) VALUES
+  ('2017-11-01', 1, 100004),
+  ('2017-11-01', 2, 100003),
+  ('2017-11-01', 0, 100005),
+  ('2017-11-02', 1, 100003),
+  ('2017-11-02', 1, 100004),
+  ('2017-11-02', 1, 100005);
+
+INSERT INTO menulists (dish, price, menu_id) VALUES
+  ('Свинная вырезка, 100г', 120.00, 100007),
+  ('Чешское светлое, 0,5л', 22, 100007),
+  ('Селедка под шубой', 55, 100008),
+  ('Молоко', 11, 100008),
+  ('Борщ классический', 45, 100006),
+  ('Макароны по флотски', 15, 100006),
+  ('Свинная вырезка, 100г', 120.95, 100009),
+  ('Чешское светлое, 0,5л', 18, 100009),
+  ('Борщ классический', 44.22, 100011),
+  ('Макароны по флотски', 15.10, 100011),
+  ('Пирог с черникой', 27, 100010),
+  ('Чай', 0.45, 100010);
+
+INSERT INTO votes (date, user_id, menu_id) VALUES
+  ('2017-11-01', 100000, 100007),
+  ('2017-11-01', 100001, 100007),
+  ('2017-11-01', 100002, 100006),
+  ('2017-11-02', 100000, 100009),
+  ('2017-11-02', 100001, 100010),
+  ('2017-11-02', 100002, 100011);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,7 +10,7 @@ CREATE SEQUENCE global_seq START 100000;
 
 CREATE TABLE users
 (
-  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  id               INTEGER DEFAULT global_seq.nextval PRIMARY KEY,
   name             VARCHAR                 NOT NULL,
   email            VARCHAR                 NOT NULL,
   password         VARCHAR                 NOT NULL,
@@ -31,38 +31,38 @@ CREATE TABLE user_roles
 
 CREATE TABLE restaurants
 (
-  id   INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  id   INTEGER DEFAULT global_seq.nextval PRIMARY KEY,
   name VARCHAR NOT NULL
 );
 CREATE UNIQUE INDEX restaurant_menu_idx
-  ON restaurant (name);
+  ON restaurants (name);
 
 CREATE TABLE menus
 (
-  id            INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  id            INTEGER DEFAULT global_seq.nextval PRIMARY KEY,
   date          DATE DEFAULT now() NOT NULL,
-  curvotenum    INTEGER             DEFAULT 0,
+  curvotenum    INTEGER DEFAULT 0,
   restaurant_id INTEGER            NOT NULL,
-  FOREIGN KEY (restaurant_id) REFERENCES restaurant (id) ON DELETE CASCADE
+  FOREIGN KEY (restaurant_id) REFERENCES restaurants (id) ON DELETE CASCADE
 );
-CREATE UNIQUE INDEX menu_date_idx
-  ON menu (date);
+CREATE UNIQUE INDEX menu_date_restaurant_idx
+  ON menus (date, restaurant_id);
 
 CREATE TABLE menulists
 (
-  id      INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  id      INTEGER DEFAULT global_seq.nextval PRIMARY KEY,
   dish    VARCHAR NOT NULL,
   price   NUMERIC(10, 4),
   menu_id INTEGER NOT NULL,
-  FOREIGN KEY (menu_id) REFERENCES menu (id) ON DELETE CASCADE
+  FOREIGN KEY (menu_id) REFERENCES menus (id) ON DELETE CASCADE
 );
 
 CREATE TABLE votes
 (
-  datetime TIMESTAMP DEFAULT now() NOT NULL,
-  user_id  INTEGER                 NOT NULL,
-  menu_id  INTEGER                 NOT NULL,
-  CONSTRAINT datetine_iser_idx UNIQUE (datetime, user_id),
+  date    DATE DEFAULT now() NOT NULL,
+  user_id INTEGER            NOT NULL,
+  menu_id INTEGER            NOT NULL,
+  CONSTRAINT date_user_idx UNIQUE (date, user_id),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
   FOREIGN KEY (menu_id) REFERENCES menus (id) ON DELETE CASCADE
 )
